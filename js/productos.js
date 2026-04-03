220fe3695705d611753638f0cee8f70ebf89d925
Gonzalo-6 <g-arranz@hotmail.com>
2026-04-03 10:11:33 +0200

    pequeñas correcciones y comprobar funcionamiento

--- js/productos.js
+++ js/productos.js
@@ -9,8 +9,8 @@ class Producto {
 }
 
 const productos = [
-    new Producto(1, "Teclado", 30, "perifericos","imagenes/teclado.jpg"),
-    new Producto(2, "Ratón", 20, "perifericos","imagenes/raton.jpg"),
-    new Producto(3, "Monitor", 200, "pantallas","imagenes/monitor.jpg"),
-    new Producto(4, "Portátil", 800, "ordenadores","imagenes/portatil.jpg")
+    new Producto(1, "Teclado", 30, "perifericos","img/teclado.jpg"),
+    new Producto(2, "Ratón", 20, "perifericos","img/raton.jpg"),
+    new Producto(3, "Monitor", 200, "pantallas","img/monitor.jpg"),
+    new Producto(4, "Portátil", 800, "ordenadores","img/portatil.jpg")
 ];
